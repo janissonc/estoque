73ad65de5733a96baa0305043a636825eb5930db
janisson <janisson.bass@gmail.com>
2019-03-14 15:11:01 -0300

    login

--- index.php
+++ index.php
@@ -1,73 +1,55 @@
-<?php 
-include_once'model/Conexao.class.php';
-include_once'model/Manager.class.php';
-$manager = new Manager();
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<?php include_once 'view/dependences.php'; ?>
-</head>
-<body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php include_once"view/dependences.php";?>
+	<style>
+		body{
+			bacground: url("view/img/fundo.jpg");
+		} 
+	</style>
+  </head>
+  <body style=" bacground: url('view/img/fundo.jpg');">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand" href="index.php">Relatório. Faça login para iniciar sua sessão</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button>
+	</nav>
+    <div class="container mt-3 bg-light " style=" width: 500px; height: 500px; padding: 10px;box-shadow: 5px 5px 5px rgba(0,0,0,0.3);">
+    	<div class="container" style="margin-top: 80px;" >
+			<form action="controller/validarconta.php" method="post">
+				<div class="row">
+				  <div class="form-group col-sm-12">
+				    <label for="exampleFormControlInput1">Login</label>
+				    <input type="text" class="form-control" id="exampleFormControlInput1" name="login">
+			      </div>
+				</div>
+				<div class="row">
+					<div class="form-group col-sm-12">
+					    <label for="exampleFormControlInput1">Senha  </label>
+					    <input type="password" class="form-control" id="exampleFormControlInput1" name="senha">
+				    </div>
+			    </div>
+				<div class="row">
+				  <div class="form-group col-sm-2">
+				    <input class="btn btn-primary" type="submit" value="Logar">
+				  </div>
+				  <div class="form-group col-sm-2">
+				    <input class="btn btn-secondary" type="reset" value="Reset">
+				  </div>
+				</div>
+			</form>
+		</div>	 
 
-<div class="container">
 	
-	<h2 class="text-center"> List of routers <i class="fa fa-users"></i></h2>
-
-	<h5 class="text-right">
-		<a href="view/router/pageRegisterRouter.php" class="btn btn-primary btn-xs">
-			<i class="fa fa-user-plus"></i>
-		</a>
-	</h5>
-
-	<!-- Iniciando a tabela -->
-
-	<div class="table-responsive">
 		
-		<table class="table table-hover">
-			<thead class="thead">
-				<tr>
-					<th>ID</th>
-					<th>Modelo</th>
-					<th>Descrição</th>
-					<th>Status</th>
-					<th colspan="3">AÇÕES</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach($manager->listData("router") as $cliente): ?>
-				<tr>
-					<td><?php echo $cliente['id']; ?></td>
-					<td><?php echo $cliente['modelo']; ?></td>
-					<td><?php echo $cliente['descricao']; ?></td>
-					<td><?php echo $cliente['status']; ?></td>
-					<td>
-						<form method="POST" action="view/router/pageUpdateRouter.php">
-							<input type="hidden" name="id" value="<?php echo $cliente['id']; ?>">
-							<button class="btn btn-warning btn-xs">
-								<i class="fa fa-user-edit"></i>
-							</button>
-						</form>
-					</td>
-					<td>
-						<form method="POST" action="controller/router/deleteRouter.php" onclick="return confirm('Tem certeza que deseja excluir ?');">
-							<input type="hidden" name="id" value="<?php echo $cliente['id']; ?>">
-							<button class="btn btn-danger btn-xs">
-								<i class="fa fa-trash"></i>
-							</button>
-						</form>
-					</td>
-				</tr>
-			<?php endforeach;?>
-			</tbody>
-		</table>
-
-	</div>
-
-	<!-- Fim da Tabela -->
-
-</div>
-
-</body>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
 </html>
+
---
+++ view/router/pageListRouter.php
@@ -0,0 +1,73 @@
+<?php 
+include_once'model/Conexao.class.php';
+include_once'model/Manager.class.php';
+$manager = new Manager();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include_once 'view/dependences.php'; ?>
+</head>
+<body>
+
+<div class="container">
+	
+	<h2 class="text-center"> List of routers <i class="fa fa-users"></i></h2>
+
+	<h5 class="text-right">
+		<a href="view/router/pageRegisterRouter.php" class="btn btn-primary btn-xs">
+			<i class="fa fa-user-plus"></i>
+		</a>
+	</h5>
+
+	<!-- Iniciando a tabela -->
+
+	<div class="table-responsive">
+		
+		<table class="table table-hover">
+			<thead class="thead">
+				<tr>
+					<th>ID</th>
+					<th>Modelo</th>
+					<th>Descrição</th>
+					<th>Status</th>
+					<th colspan="3">AÇÕES</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach($manager->listData("router") as $cliente): ?>
+				<tr>
+					<td><?php echo $cliente['id']; ?></td>
+					<td><?php echo $cliente['modelo']; ?></td>
+					<td><?php echo $cliente['descricao']; ?></td>
+					<td><?php echo $cliente['status']; ?></td>
+					<td>
+						<form method="POST" action="view/router/pageUpdateRouter.php">
+							<input type="hidden" name="id" value="<?php echo $cliente['id']; ?>">
+							<button class="btn btn-warning btn-xs">
+								<i class="fa fa-user-edit"></i>
+							</button>
+						</form>
+					</td>
+					<td>
+						<form method="POST" action="controller/router/deleteRouter.php" onclick="return confirm('Tem certeza que deseja excluir ?');">
+							<input type="hidden" name="id" value="<?php echo $cliente['id']; ?>">
+							<button class="btn btn-danger btn-xs">
+								<i class="fa fa-trash"></i>
+							</button>
+						</form>
+					</td>
+				</tr>
+			<?php endforeach;?>
+			</tbody>
+		</table>
+
+	</div>
+
+	<!-- Fim da Tabela -->
+
+</div>
+
+</body>
+</html>
